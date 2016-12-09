b89ccc44c2d1cbe5795f3cc9424b7b2b680bf9b5
Myles Ieong <Myles Ieong>
2016-12-09 21:40:14 +0800

    fix the previous cast problem with using the right import class package

--- app/src/main/java/com/myles/udacity/newsapp/MainActivity.java
+++ app/src/main/java/com/myles/udacity/newsapp/MainActivity.java
@@ -4,8 +4,8 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -46,7 +46,11 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         // Set Empty view for listview
         ((ListView) this.findViewById(R.id.list)).setEmptyView((TextView) findViewById(R.id.empty_view));
 
+        LoaderManager loaderManager = this.getLoaderManager();
+        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+
         //Setup button onclick listener to activate the task
+        /**
         ((Button) findViewById(R.id.button_search)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,13 +64,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
             }
         });
-
-
+         */
     }
 
     @Override
     public Loader<List<Book>> onCreateLoader(int id, Bundle args) {
-        String searchQuery = ((TextView) findViewById(R.id.text_input)).getText().toString();
+        //String searchQuery = ((TextView) findViewById(R.id.text_input)).getText().toString();
+        String searchQuery = "android";
         URL searchUrl = null;
         try {
             searchUrl = new URL(BOOK_API_REQUEST_URL + searchQuery);
